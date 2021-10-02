8250eceeaf5ccf7a0cae956cdab96e3ad90d7070
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-10-02 16:45:43 +0300

    Corrected the code to the standard PHPMD and PHPCS

--- registration.php
+++ registration.php
@@ -2,7 +2,8 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE,
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'Nofarius1992_Blog',
     __DIR__
 );
