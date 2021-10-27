f4b5462348138c2686b2cd2968ee04a5cce839c6
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-10-27 14:27:18 +0300

    Edited the code.

--- Controller/Adminhtml/Categories/Index.php
+++ Controller/Adminhtml/Categories/Index.php
@@ -7,22 +7,17 @@ namespace Nofarius1992\Blog\Controller\Adminhtml\Categories;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\PageFactory;
 
-/**
- * Index action.
- */
 class Index implements HttpGetActionInterface
 {
-    const MENU_ID = 'Nofarius1992_Blog::blog_categories';
+    private const MENU_ID = 'Nofarius1992_Blog::blog_categories';
 
-    protected PageFactory $resultPageFactory;
+    private PageFactory $resultPageFactory;
 
     /**
      * @inheritDoc
      */
-    public function __construct(
-        PageFactory $resultPageFactory
-    ) {
-
+    public function __construct(PageFactory $resultPageFactory)
+    {
         $this->resultPageFactory = $resultPageFactory;
     }
 
