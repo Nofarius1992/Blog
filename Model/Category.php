3fef4c57f3d7ea1d8c458d048caaa30a0d9ca5dd
Nofarius1992 <tolya.prozhoga@gmail.com>
2021-10-27 11:03:16 +0300

    Set declare (strict _types = 1) on all files. Added description to classes and @inheritDoc.

--- Model/Category.php
+++ Model/Category.php
@@ -1,12 +1,19 @@
 <?php
+declare(strict_types=1);
 
 namespace Nofarius1992\Blog\Model;
 
 use Nofarius1992\Blog\Model\ResourceModel\Category as ResourceModel;
 use Magento\Framework\Model\AbstractModel;
 
+/**
+ * Blog category model
+ */
 class Category extends AbstractModel
 {
+    /**
+     * @inheritDoc
+     */
     protected function _construct()
     {
         $this->_init(ResourceModel::class);
--- Model/ResourceModel/Category/Collection.php
+++ Model/ResourceModel/Category/Collection.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Nofarius1992\Blog\Model\ResourceModel\Category;
 
@@ -6,8 +7,14 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Nofarius1992\Blog\Model\Category as Model;
 use Nofarius1992\Blog\Model\ResourceModel\Category as ResourceModel;
 
+/**
+ * Blog category collection
+ */
 class Collection extends AbstractCollection
 {
+    /**
+     * @inheritDoc
+     */
     protected function _construct()
     {
         $this->_init(Model::class, ResourceModel::class);
